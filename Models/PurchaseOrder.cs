a1dc5688c46bef6a7edcc5a00c40d149b60d4609
nguynbui <nguyentoq4gou@gmail.com>
2026-03-14 21:31:49 +0700

    Add project files.

---
+++ Models/PurchaseOrder.cs
@@ -0,0 +1,9 @@
+﻿namespace Final.Models;
+
+public class PurchaseOrder
+{
+    public string PurchaseOrderID { get; set; } = string.Empty;
+    public string PurchaseOrderNumber { get; set; } = string.Empty;
+    public DateTime Date { get; set; }
+    public decimal Total { get; set; }
+}

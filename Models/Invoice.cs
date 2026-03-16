9a35c72a38074bbb0fcf05f47dea64b439d160b3
nguynbui <nguyentoq4gou@gmail.com>
2026-03-16 16:19:45 +0700

    Delete Models directory

--- Models/Invoice.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace Final.Models;
-
-public class Invoice
-{
-    public string InvoiceID { get; set; } = string.Empty;
-    public string InvoiceNumber { get; set; } = string.Empty;
-    public DateTime Date { get; set; }
-    public decimal Total { get; set; } // Sử dụng decimal cho tiền tệ
-}
--- Models/PurchaseOrder.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace Final.Models;
-
-public class PurchaseOrder
-{
-    public string PurchaseOrderID { get; set; } = string.Empty;
-    public string PurchaseOrderNumber { get; set; } = string.Empty;
-    public DateTime Date { get; set; }
-    public decimal Total { get; set; }
-}

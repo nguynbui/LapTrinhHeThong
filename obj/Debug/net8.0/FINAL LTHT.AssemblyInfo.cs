c25d8397028b825f30422b6694bd8859ca44a2ec
HuyVuCS <dotranhuyvu@.com>
2026-03-17 16:26:33 +0700

    update

--- obj/Debug/net8.0/FINAL LTHT.AssemblyInfo.cs
+++ obj/Debug/net8.0/FINAL LTHT.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("FINAL LTHT")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+933bfab1cea98d9362a6927f7ce800fe71b4d81c")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0f7f14de6324158e1508efef1f0de43b2c19d0ef")]
 [assembly: System.Reflection.AssemblyProductAttribute("FINAL LTHT")]
 [assembly: System.Reflection.AssemblyTitleAttribute("FINAL LTHT")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
